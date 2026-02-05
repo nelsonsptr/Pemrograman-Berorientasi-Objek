c14a2c00624021d73a5bd59981dcafe8f9881528
nelsonputra <nelson.putra.id@gmail.com>
2026-02-05 14:25:47 +0700

    Pertemuan 1

---
+++ KodePertama.java
@@ -0,0 +1,11 @@
+public class KodePertama {
+    public static void main(String[] args){
+        System.out.println("Halo, Pemrograman Berorientasi Objek!");
+        System.out.println("Ini adalah program Java pertama saya");
+    }
+}
+/*
+Nelson Saputra
+D1041241003
+5 Februari 2026
+ */
---
+++ NelsonSaputra_D1041241003_Semester4.java
@@ -0,0 +1,13 @@
+public class NelsonSaputra_D1041241003_Semester4{
+    public static void main(String[] args) {
+        System.out.println("Nama : Nelson Saputra");
+        System.out.println("NIM : D1041241003");
+        System.out.println("Semester 4");
+        System.out.println("Prodi : Informatika");
+    }
+}
+/*
+Nelson Saputra
+D1041241003
+5 Februari 2026
+ */
