893c693e29f6810f1c63bff6ce7ff967463a1801
nelsonputra <nelson.putra.id@gmail.com>
2026-02-12 13:51:03 +0700

    Pertemuan 3

--- Pertemuan 3/p023PrimitiveData.java
+++ Pertemuan 3/p023PrimitiveData.java
@@ -7,6 +7,20 @@ public class p023PrimitiveData {
         //Tipe Data Bilangan Desimal
         float tinggi = 170.5f;
         double ipk = 3.75;
+
+        //Tipe Data Karakter
+        char nilaiHuruf = 'A';
+
+        //Tipe Data Logika
+        boolean lulus = true;
+
+        //Menampilkan Nilai Semua Variable
+        System.out.println("Umur            : " + umur);
+        System.out.println("Populasi        : " + populasi);
+        System.out.println("Tinggi badan    : " + tinggi);
+        System.out.println("IPK             : " + ipk);
+        System.out.println("Nilai Huruf     : " + nilaiHuruf);
+        System.out.println("Status Lulus    : " + lulus);
     }
 }
 /*
