e9ad94ebae5756312617acb578c2cdb96d660768
nelsonputra <nelson.putra.id@gmail.com>
2026-02-12 13:44:52 +0700

    Pertemuan 3

---
+++ Pertemuan 3/p022WorkingVariable.java
@@ -0,0 +1,16 @@
+public class p022WorkingVariable{
+    public static void main(String[] args) {
+        //Deklarasi dan Inisialisasi Variable
+        int angka = 10;
+        //Menampilkan Nilai Awal Variable
+        System.out.println("Nilai Awal Angka: " + angka);
+        //Mengubah Nilai Variable
+        angka = 25;
+        //Menampilkan Nilai Setelah Perubahan
+        System.out.println("Nilai Angka Setelah Diubah: " + angka);
+    }
+}
+/*
+Nama / NIM : Nelson Saputra / D1041241003
+Tanggal : 12 Februari 2026
+*/
---
+++ Pertemuan 3/p023PrimitiveData.java
@@ -0,0 +1,15 @@
+public class p023PrimitiveData {
+    public static void main(String[] args) {
+        //Tipe Data Bilangan Bulat
+        int umur = 20;
+        long populasi = 8000000000L;
+
+        //Tipe Data Bilangan Desimal
+        float tinggi = 170.5f;
+        double ipk = 3.75;
+    }
+}
+/*
+Nama / NIM : Nelson Saputra / D1041241003
+Tanggal : 12 Februari 2026
+*/
